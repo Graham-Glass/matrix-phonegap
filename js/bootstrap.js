62564f53608a451c35ab55baafc53d20af4acca9
sergiu-nichitean <sergiu.nichitean@artsoft-consult.ro>
2015-10-09 11:07:48 +0300

    switch to live

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,11 +1,11 @@
 var appVersion = '1.03';
 
 var store = window.localStorage;
-var domain = 'edu-20.com';
-//var domain = 'www.matrixlms.com';
+//var domain = 'edu-20.com';
+var domain = 'www.matrixlms.com';
 //var domain = 'www.neolms.com';
-var domainProtocol = 'http';
-//var domainProtocol = 'https';
+//var domainProtocol = 'http';
+var domainProtocol = 'https';
 var loginUrl = domainProtocol + '://' + domain + '/app/launch?version=' + appVersion + '&mobile_app=true&left_nav=true&new_left_nav=true&redirected=true&new_jwplayer=true';
 var isFirst = true;
 var wasOffline = false;
